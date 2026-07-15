1a7492d902d978e09fb78846b790b83284377295
Sakshi Maholiya <122376204+SakshiMaholiya123@users.noreply.github.com>
2026-07-15 16:58:40 +0530

    Delete chatmodels directory

--- chatmodels/localmodel.py
+++
@@ -1,9 +0,0 @@
-from langchain_huggingface import ChatHuggingFace,HuggingFacePipeline
-
-llm=HuggingFacePipeline.from_model_id(model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",task="text-generation")
-
-chat_model=ChatHuggingFace(llm=llm)
-
-res=chat_model.invoke("what is data science?")
-
-print(res.content)
